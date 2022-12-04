89789541de9050d69a17aa394843ca459e4df8e5
Elif Kose <kose.elif@hotmail.com>
2022-12-04 00:54:52 -0500

    Framework is completed

---
+++ src/test/java/com/cydeo/Runner/CukesRunner.java
@@ -0,0 +1,22 @@
+package com.cydeo.Runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        plugin = {
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt" ,
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features" ,
+        glue = "com/cydeo/Steps",
+        dryRun = false,
+        tags = "@smoke"
+)
+public class CukesRunner {
+
+}
